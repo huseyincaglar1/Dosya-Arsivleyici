469e19e97d56147fd692c21ffb7d00adc4337834
huseyincaglar1 <huseyincaglar309@gmail.com>
2025-01-02 15:58:33 +0300

    commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.huseyin.deneme {
     requires javafx.controls;
     requires javafx.fxml;
-    requires jdk.jartool; 
+    requires jdk.jartool; // Bu modülü kullanıyorsanız bırakabilirsiniz
 
     opens org.huseyin.deneme to javafx.fxml;
     exports org.huseyin.deneme;
